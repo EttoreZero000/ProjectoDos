7cb9294fa29ef346d93db892bbed2f726a1237e5
Hector Leiva Gamboa <hectorleivagamboa0741@gmail.com>
2024-11-03 17:16:35 -0600

    3x3 con nodos.dato int

--- main.cpp
+++ main.cpp
@@ -1,119 +1,103 @@
 #include <QApplication>
 #include <QWidget>
-#include <QPushButton>
-#include <QVector>
-#include <QString>
-#include <QDebug>
 #include <QPainter>
-#include <QResizeEvent>
+#include <QVector>
 
 class Nodo {
 public:
-    int dato;
-    QVector<Nodo*> vecinos;
-    QPoint pos; // Posición del nodo en el tablero
+    int dato; // Dato del nodo
+    QVector<Nodo*> vecinos; // Lista de nodos conectados
 
-    Nodo(int d, const QPoint& position) : dato(d), pos(position) {}
+    Nodo(int d) : dato(d) {} // Constructor
 };
 
-class Tablero : public QWidget {
+class GrafoWidget : public QWidget {
+    QVector<Nodo*> nodos; // Lista de nodos en el grafo
+
 public:
-    Tablero(QWidget* parent = nullptr) 
-        : QWidget(parent) {
-        setMinimumSize(400, 400);
-        setWindowTitle("Juego de Tablero");
-
-        // Crear nodos
-        for (int i = 0; i < 5; ++i) {
-            Nodo* nodo = new Nodo(i, QPoint(0, 0)); // Inicializa con una posición nula
-            nodos.append(nodo);
-
-            QPushButton* boton = new QPushButton(QString::number(nodo->dato), this);
-            boton->setGeometry(0, 0, 60, 30); // Inicializa con geometría nula
-            connect(boton, &QPushButton::clicked, [=]() { manejarClick(nodo); });
-        }
-
-        conectarNodos();
-        actualizarPosiciones(); // Establecer las posiciones iniciales
+    GrafoWidget() {
+        // Inicializar el grafo
+        initializeGraph();
     }
 
 protected:
-    void resizeEvent(QResizeEvent* event) override {
-        QWidget::resizeEvent(event);
-        actualizarPosiciones(); // Actualiza las posiciones al redimensionar
-    }
+    void paintEvent(QPaintEvent *event) override {
+        Q_UNUSED(event);
 
-    void paintEvent(QPaintEvent* event) override {
         QPainter painter(this);
-        painter.setPen(Qt::black); // Color de la línea
-
-        for (Nodo* nodo : nodos) {
-            // Dibuja líneas hacia los nodos vecinos
-            for (Nodo* vecino : nodo->vecinos) {
-                painter.drawLine(nodo->pos.x() + 30, nodo->pos.y() + 15, 
-                                 vecino->pos.x() + 30, vecino->pos.y() + 15);
-            }
-        }
-    }
-
-private:
-    QVector<Nodo*> nodos;
-
-    void conectarNodos() {
-        nodos[0]->vecinos.append(nodos[1]);
-        nodos[0]->vecinos.append(nodos[2]);
-        nodos[0]->vecinos.append(nodos[4]);
-
-        nodos[1]->vecinos.append(nodos[0]);
-        nodos[1]->vecinos.append(nodos[3]);
-
-        nodos[2]->vecinos.append(nodos[0]);
-        nodos[2]->vecinos.append(nodos[3]);
-
-        nodos[3]->vecinos.append(nodos[1]);
-        nodos[3]->vecinos.append(nodos[2]);
-        nodos[3]->vecinos.append(nodos[4]);
-
-        nodos[4]->vecinos.append(nodos[0]);
-        nodos[4]->vecinos.append(nodos[3]);
-    }
-
-    void actualizarPosiciones() {
+        
+        // Tamaño de la ventana
         int width = this->width();
-        int height = this->height();
+        int height = this->height() - 60; // Ajustar altura si es necesario
+        
+        // Coordenadas centradas de los nodos
+        QPoint nodes[9] = {
+            QPoint(width / 3, height / 20), // 0
+            QPoint(width / 2, height / 20),   // 1
+            QPoint(width * 2/3, height / 20), // 2
 
-        // Definir nuevas posiciones de los nodos en relación con el tamaño de la ventana
-        QVector<QPoint> posiciones = {
-            QPoint(width / 4, height / 2),   // Nodo 0
-            QPoint(width / 2, height / 4),   // Nodo 1
-            QPoint(3 * width / 4, height / 2), // Nodo 2
-            QPoint(width / 2, 3 * height / 4), // Nodo 3
-            QPoint(width / 4, 3 * height / 4) // Nodo 4
+            QPoint(width / 3, height / 7),   // 3
+            QPoint(width / 2, height / 7), // 4
+            QPoint(width * 2/3, height / 7), // 5
+
+            QPoint(width / 3, height / 4),// 6
+            QPoint(width / 2, height / 4),// 7
+            QPoint(width * 2/3, height / 4)// 8
         };
 
-        // Actualizar la posición de los nodos y los botones
-        for (int i = 0; i < nodos.size(); ++i) {
-            nodos[i]->pos = posiciones[i];
-
-            // Actualizar la geometría del botón
-            QPushButton* boton = findChild<QPushButton*>(QString::number(i));
-            if (boton) {
-                boton->setGeometry(posiciones[i].x(), posiciones[i].y(), 60, 30);
+        // Dibujar las conexiones entre nodos
+        painter.setPen(Qt::black);
+        for (Nodo* nodo : nodos) {
+            for (Nodo* vecino : nodo->vecinos) {
+                int x1 = nodes[nodo->dato].x();
+                int y1 = nodes[nodo->dato].y();
+                int x2 = nodes[vecino->dato].x();
+                int y2 = nodes[vecino->dato].y();
+                painter.drawLine(x1, y1, x2, y2);
             }
         }
 
-        update(); // Pinta de nuevo el widget
+        // Dibujar cada nodo (círculo)
+        for (int i = 0; i < nodos.size(); i++) {
+            painter.setBrush(Qt::lightGray);
+            painter.drawEllipse(nodes[i].x() - 10, nodes[i].y() - 10, 20, 20);
+            
+            // Dibujar número dentro del nodo
+            painter.drawText(nodes[i].x() - 5, nodes[i].y() + 5, QString::number(nodos[i]->dato));
+        }
     }
 
-    void manejarClick(Nodo* nodo) {
-        qDebug() << "Nodo clicado:" << nodo->dato;
+    void initializeGraph() {
+        // Crear nodos
+        for (int i = 0; i < 9; i++) {
+            nodos.append(new Nodo(i)); // Crear nodos con valores de 0 a 6
+        }
+
+        // Inicializa el grafo con conexiones (nodos adyacentes)
+        nodos[0]->vecinos = {nodos[1], nodos[3], nodos[4]};
+        nodos[1]->vecinos = {nodos[0], nodos[2], nodos[4]};
+        nodos[2]->vecinos = {nodos[1], nodos[4], nodos[5]};
+        nodos[3]->vecinos = {nodos[0], nodos[4], nodos[6]};
+        nodos[4]->vecinos = {nodos[0], nodos[1], nodos[2], nodos[3], nodos[5], nodos[6], nodos[7], nodos[8]};
+        nodos[5]->vecinos = {nodos[2], nodos[4], nodos[8]};
+        nodos[6]->vecinos = {nodos[3], nodos[4], nodos[7]};
+        nodos[7]->vecinos = {nodos[4], nodos[6], nodos[8]};
+        nodos[8]->vecinos = {nodos[4], nodos[5], nodos[7]};
+    }
+public:
+    ~GrafoWidget() {
+        // Liberar memoria de los nodos
+        qDeleteAll(nodos);
     }
 };
 
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-
-    Tablero tablero;
-    tablero.show();
+    
+    GrafoWidget window;
+    window.resize(400, 400);
+    window.setWindowTitle("Grafo Simple");
+    window.show();
+    
     return app.exec();
 }
