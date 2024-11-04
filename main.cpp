4613668c7b05380c265709156fc68a69f80e496b
Hector Leiva Gamboa <hectorleivagamboa0741@gmail.com>
2024-11-03 18:06:46 -0600

    Falta realizar mas nodos

--- main.cpp
+++ main.cpp
@@ -7,83 +7,96 @@ class Nodo {
 public:
     int dato; // Dato del nodo
     QVector<Nodo*> vecinos; // Lista de nodos conectados
+    QPoint posicion; // Posición del nodo
 
     Nodo(int d) : dato(d) {} // Constructor
 };
 
 class GrafoWidget : public QWidget {
     QVector<Nodo*> nodos; // Lista de nodos en el grafo
+    int nodeCount = 11; // Número de nodos
 
 public:
     GrafoWidget() {
-        // Inicializar el grafo
         initializeGraph();
     }
 
 protected:
     void paintEvent(QPaintEvent *event) override {
         Q_UNUSED(event);
-
         QPainter painter(this);
-        
-        // Tamaño de la ventana
+
+        // Calcular posiciones de los nodos según el tamaño de la ventana
         int width = this->width();
         int height = this->height() - 60; // Ajustar altura si es necesario
-        
-        // Coordenadas centradas de los nodos
-        QPoint nodes[9] = {
-            QPoint(width / 3, height / 20), // 0
-            QPoint(width / 2, height / 20),   // 1
-            QPoint(width * 2/3, height / 20), // 2
-
-            QPoint(width / 3, height / 7),   // 3
-            QPoint(width / 2, height / 7), // 4
-            QPoint(width * 2/3, height / 7), // 5
-
-            QPoint(width / 3, height / 4),// 6
-            QPoint(width / 2, height / 4),// 7
-            QPoint(width * 2/3, height / 4)// 8
-        };
+        setNodePositions(width, height);
 
         // Dibujar las conexiones entre nodos
         painter.setPen(Qt::black);
         for (Nodo* nodo : nodos) {
             for (Nodo* vecino : nodo->vecinos) {
-                int x1 = nodes[nodo->dato].x();
-                int y1 = nodes[nodo->dato].y();
-                int x2 = nodes[vecino->dato].x();
-                int y2 = nodes[vecino->dato].y();
-                painter.drawLine(x1, y1, x2, y2);
+                painter.drawLine(nodo->posicion, vecino->posicion);
             }
         }
 
         // Dibujar cada nodo (círculo)
-        for (int i = 0; i < nodos.size(); i++) {
+        for (Nodo* nodo : nodos) {
             painter.setBrush(Qt::lightGray);
-            painter.drawEllipse(nodes[i].x() - 10, nodes[i].y() - 10, 20, 20);
-            
-            // Dibujar número dentro del nodo
-            painter.drawText(nodes[i].x() - 5, nodes[i].y() + 5, QString::number(nodos[i]->dato));
+            painter.drawEllipse(nodo->posicion.x() - 10, nodo->posicion.y() - 10, 20, 20);
+            painter.drawText(nodo->posicion.x() - 5, nodo->posicion.y() + 5, QString::number(nodo->dato));
         }
     }
 
     void initializeGraph() {
         // Crear nodos
-        for (int i = 0; i < 9; i++) {
-            nodos.append(new Nodo(i)); // Crear nodos con valores de 0 a 6
+        for (int i = 0; i < nodeCount; i++) {
+            nodos.append(new Nodo(i));
         }
 
-        // Inicializa el grafo con conexiones (nodos adyacentes)
-        nodos[0]->vecinos = {nodos[1], nodos[3], nodos[4]};
-        nodos[1]->vecinos = {nodos[0], nodos[2], nodos[4]};
-        nodos[2]->vecinos = {nodos[1], nodos[4], nodos[5]};
-        nodos[3]->vecinos = {nodos[0], nodos[4], nodos[6]};
-        nodos[4]->vecinos = {nodos[0], nodos[1], nodos[2], nodos[3], nodos[5], nodos[6], nodos[7], nodos[8]};
-        nodos[5]->vecinos = {nodos[2], nodos[4], nodos[8]};
-        nodos[6]->vecinos = {nodos[3], nodos[4], nodos[7]};
-        nodos[7]->vecinos = {nodos[4], nodos[6], nodos[8]};
-        nodos[8]->vecinos = {nodos[4], nodos[5], nodos[7]};
+        // Definir las conexiones usando una matriz de adyacencia
+        int adjacencyMatrix[11][11] = {
+            {0, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0},
+            {1, 0, 1, 0, 1, 1, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0},
+            {1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0},
+            {1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0},
+            {0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 1, 0, 1, 0, 1, 0, 0},
+            {0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+        };
+
+        // Inicializa las conexiones a partir de la matriz de adyacencia
+        for (int i = 0; i < nodeCount; i++) {
+            for (int j = 0; j < nodeCount; j++) {
+                if (adjacencyMatrix[i][j] == 1) {
+                    nodos[i]->vecinos.append(nodos[j]);
+                }
+            }
+        }
     }
+
+    void setNodePositions(int width, int height) {
+        // Calcular posiciones de los nodos de manera responsiva
+        // Puedes ajustar estos valores para crear la disposición que prefieras
+        nodos[0]->posicion = QPoint(width / 3, height / 8);
+        nodos[1]->posicion = QPoint(width / 2, height / 8);
+        nodos[2]->posicion = QPoint(width * 2/3, height / 8);
+
+        nodos[3]->posicion = QPoint(width / 3, height / 4);
+        nodos[4]->posicion = QPoint(width / 2, height / 4);
+        nodos[5]->posicion = QPoint(width * 2/3, height / 4);
+
+        nodos[6]->posicion = QPoint(width / 3, 3 * height / 8);
+        nodos[7]->posicion = QPoint(width / 2, 3 * height / 8);
+        nodos[8]->posicion = QPoint(width * 2/3, 3 * height / 8);
+
+        nodos[9]->posicion = QPoint(width * 0.1, 3 * height / 8);
+        nodos[10]->posicion = QPoint(width * 0.2, 3 * height / 8);
+    }
+
 public:
     ~GrafoWidget() {
         // Liberar memoria de los nodos
